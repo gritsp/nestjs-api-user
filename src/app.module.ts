c29cf43d5661491a58e32f0d59302ba5d0217566
Gritsanapong Punti <gritsanapongp@gmail.com>
2023-07-13 22:21:47 +0700

    add hello world

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseConfigModule } from './mongoose.module';
 import { authMiddleware } from './middleware/auth.middleware';
-
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 @Module({
   imports: [MongooseConfigModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
